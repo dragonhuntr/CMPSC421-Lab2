3933da98ebab7358e2aea8bc2609f8439e0d1e7f
Limit <31814492+dragonhuntr@users.noreply.github.com>
2025-03-16 16:58:42 -0500

    add swagger options

--- index.js
+++ index.js
@@ -11,8 +11,30 @@ mongoose.connect(process.env.MONGODB_URI)
     .then(() => console.log('Connected to MongoDB'))
     .catch(err => console.error('MongoDB connection error:', err));
 
+// swagger
+const swaggerOptions = {
+    definition: {
+        openapi: '3.0.0',
+        info: {
+            title: 'RESTful API',
+            version: '1.0.0',
+        },
+        servers: [
+            {
+                url: `http://localhost:${process.env.PORT}`,
+            },
+        ],
+    },
+    apis: ['./routes/*.js', './models/*.js'],
+};
+
+const swaggerDocs = swaggerJsDoc(swaggerOptions);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
+
 // express routes
 app.use('/api/customers', require('./routes/customerRoutes'));
+app.use('/api/orders', require('./routes/orderRoutes'));
+app.use('/api/payments', require('./routes/paymentRoutes'));
 
 // error handling
 app.use((err, req, res, next) => {
