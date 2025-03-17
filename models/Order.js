9ea6ed40d8e384ff82c3d54dad449959eb0754bb
Limit <31814492+dragonhuntr@users.noreply.github.com>
2025-03-16 20:24:31 -0500

    add cancelled status to order

--- models/Order.js
+++ models/Order.js
@@ -64,7 +64,7 @@ const orderSchema = new mongoose.Schema({
     },
     status: {
         type: String,
-        enum: ['pending', 'completed'],
+        enum: ['pending', 'completed', 'cancelled'],
         default: 'pending',
     },
     paymentStatus: {
